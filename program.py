93fc44f27b3e03fc9642ee2f89e49c058791ffda
Alex Boestad <256887b@student.gla.ac.uk>
2022-04-27 15:44:03 +0100

    Added basic logic for dedup

--- program.py
+++ program.py
@@ -1,32 +1,50 @@
 from distutils.log import error
 
+def openFile():
+    global fileName
+    global fullFileName
+    fileName = input("File name without file extension (txt): ")
+    fullFileName = fileName + ".txt"
+    f = open(fullFileName, 'r')
+    return f
+
 def splitFunc():
-        fileName = input("File name without file extension (txt): ")
-        split = input("Split by how much (int): ")
-        newFile = fileName + "_splitby_" + split
-        fileName = fileName + ".txt"
+    openFile()
+    split = input("Split by how much (int): ")
 
-        f = open(fileName, 'r')
-        wr = open(newFile, 'x')
+    newFile = fileName + "_splitby_" + split
+    wr = open(newFile, 'x')
 
+    nl = f.readline()
+    c = 1
+    c2 = 0
+
+    while nl != '':  
+        if c == int(split):
+            wr.write(nl)
+            c = 1
+        if c2 % 1000 == 0:
+            print("Scanned " + str(c2) + "lines.")
+        c += 1
+        c2 += 1
         nl = f.readline()
-        c = 1
-        c2 = 0
+    print("Scanned " + str(c2) + "lines.")
+    print("New file created called " + newFile + ".")
 
-        while nl != '':  
-            if c == int(split):
-                wr.write(nl)
-                c = 1
-            if c2 % 1000 == 0:
-                print("Scanned " + str(c2) + "lines.")
-            c += 1
-            c2 += 1
-            nl = f.readline()
-        print("Scanned " + str(c2) + "lines.")
-        print("New file created called " + newFile + ".")
+    f.close()
+    wr.close()
 
-        f.close()
-        wr.close()
+def dedupFunc():
+    
+    openFile()
+    tempList = []
+    nl = f.readline()
+    while nl != "":
+        tempList.append(nl)
+        nl = f.readline()
+    
+    
+
 
 
 
